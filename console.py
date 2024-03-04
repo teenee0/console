4059f832b2cc28f6715adb5919929f0e47288aa5
teenee0 <qwerty.gogi@mail.ru>
2024-03-04 04:13:31 +0300

    version 1.1

--- console.py
+++ console.py
@@ -1,15 +1,13 @@
 import os
 import shutil
 
-# Функция для создания папки
+
 def create_folder(folder_name):
     try:
         os.mkdir(folder_name)
         print(f'Папка "{folder_name}" создана успешно')
     except FileExistsError:
         print(f'Папка "{folder_name}" уже существует')
-
-# Функция для удаления папки
 def delete_folder(folder_name):
     try:
         shutil.rmtree(folder_name)
@@ -17,7 +15,7 @@ def delete_folder(folder_name):
     except FileNotFoundError:
         print(f'Папка "{folder_name}" не существует')
 
-# Функция для перемещения между папками
+
 def change_directory(folder_name):
     try:
         os.chdir(folder_name)
@@ -27,19 +25,19 @@ def change_directory(folder_name):
         print(f'Папка "{folder_name}" не существует')
         return False
 
-# Функция для создания пустого файла
+
 def create_file(file_name):
     with open(file_name, 'w') as file:
         pass
     print(f'Файл "{file_name}" создан успешно')
 
-# Функция для записи текста в файл
+
 def write_to_file(file_name, text):
     with open(file_name, 'w') as file:
         file.write(text)
     print(f'Текст записан в файл "{file_name}"')
 
-# Функция для просмотра содержимого текстового файла
+
 def read_file(file_name):
     try:
         with open(file_name, 'r') as file:
@@ -47,7 +45,7 @@ def read_file(file_name):
     except FileNotFoundError:
         print(f'Файл "{file_name}" не существует')
 
-# Функция для удаления файла
+
 def delete_file(file_name):
     try:
         os.remove(file_name)
@@ -55,7 +53,7 @@ def delete_file(file_name):
     except FileNotFoundError:
         print(f'Файл "{file_name}" не существует')
 
-# Функция для копирования файла
+
 def copy_file(source, destination):
     try:
         shutil.copy(source, destination)
@@ -63,7 +61,7 @@ def copy_file(source, destination):
     except FileNotFoundError:
         print('Не удалось скопировать файл')
 
-# Функция для перемещения файла
+
 def move_file(source, destination):
     try:
         shutil.move(source, destination)
@@ -71,7 +69,6 @@ def move_file(source, destination):
     except FileNotFoundError:
         print('Не удалось переместить файл')
 
-# Функция для переименования файла
 def rename_file(old_name, new_name):
     try:
         os.rename(old_name, new_name)
@@ -81,23 +78,14 @@ def rename_file(old_name, new_name):
 
 def show_directory(my_folder, directory):
     a = current_directory().split("\\").index(my_folder)
-    # print(1, directory)
-    # print(current_directory().split("\\"))
     b = current_directory().split("\\").index(directory)
-    # print(a,b)
+
     mass = current_directory().split("\\")[a:b+1]
 
     for i in range(len(mass)):
         print(f'¬{mass[i]}')
         print("    " * (i+1), end="")
-    print()
-
-
-    # mass = current_directory().split("\\")[-2] папка где код
-
-    # print(mass)
-
-
+    print(end="\n")
 
 def show_files_in_current_directory():
     current_dir = os.getcwd()
@@ -124,10 +112,28 @@ def previous_directory():
 def current_directory():
     return os.getcwd()
 
+
+def info():
+    print('\nДоступные действия:')
+    print('sh_fi. Показать файлы в директории')
+    print('sh_fo. Показать папки в директории')
+    print('pr_di. Возвращение в предыдущую директорию')
+    print('cr_fo. Создать папку')
+    print('de_fo. Удалить папку')
+    print('ch_dr. Перейти в папку')
+    print('cr_fi. Создать файл')
+    print('wr_to_fi. Записать текст в файл')
+    print('re_fi. Просмотреть содержимое файла')
+    print('de_fi. Удалить файл')
+    print('co_fi. Копировать файл')
+    print('mo_fi. Переместить файл')
+    print('re_fi. Переименовать файл')
+    print('q. Выйти')
+
+
 def main():
     my_folder = current_directory().split("\\")[-1]
     print(f'Корневая папка кода - {my_folder}')
-    # print("Создаем папку для теста...")
     if not os.path.exists("test"):
         print("Создаем папку для теста...")
         os.makedirs("test")
@@ -135,40 +141,29 @@ def main():
     else:
         print(f'Папка "{"test"}" уже существует')
     change_directory("test")
-    # show_directory(my_folder, "test")
-    # show_files_in_current_directory()
     now_dir = 'test'
     my_folder = current_directory().split("\\")[-1]
+    show_directory(my_folder, now_dir)
     while True:
-
-        show_directory(my_folder, now_dir)
         print(os.getcwd())
-        # print('\nДоступные действия:')
-        # print('cr_fo. Создать папку')
-        # print('de_fo. Удалить папку')
-        # print('ch_dr. Перейти в папку')
-        # print('cr_fi. Создать файл')
-        # print('wr_to_fi. Записать текст в файл')
-        # print('re_fi. Просмотреть содержимое файла')
-        # print('de_fi. Удалить файл')
-        # print('co_fi. Копировать файл')
-        # print('mo_fi. Переместить файл')
-        # print('re_fi. Переименовать файл')
-        # print('q. Выйти')
-
+        show_directory(my_folder, now_dir)
+        print("-" * 30)
         choice = input('Выберите действие: ')
-        if choice == "1":
-            show_directory(my_folder, now_dir)
+        print("-"*30)
+        if choice == "sh_fi":
+            # show_directory(my_folder, now_dir)
             show_files_in_current_directory()
-        elif choice == "2":
+        elif choice == "sh_fo":
             show_folders_in_current_directory()
-        elif choice == "3":
+        elif choice == "pr_di":
             if now_dir == my_folder:
-                print("u cant")
+                print("Это коренная папка!")
                 continue
             else:
                 now_dir = previous_directory()
-
+                print("Возвращение в предыдущую директорию")
+        elif choice == "info":
+            info()
         elif choice == 'cr_fo':
             folder_name = input('Введите имя папки для создания: ')
             create_folder(folder_name)
@@ -209,6 +204,8 @@ def main():
             break
         else:
             print('Некорректный ввод, попробуйте снова.')
+        print("-" * 30)
+
 
         
 
